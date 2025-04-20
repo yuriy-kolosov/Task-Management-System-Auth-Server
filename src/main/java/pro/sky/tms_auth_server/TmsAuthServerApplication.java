b21ceff976e4c4d4d763330dd296b4f6cb73ab75
yuriy-kolosov <yuriy.v.kolosov@gmail.com>
2025-04-20 23:06:44 +0300

    Task Management System Authorization Server - начальный коммит

---
+++ src/main/java/pro/sky/tms_auth_server/TmsAuthServerApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.tms_auth_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmsAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmsAuthServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/tms_auth_server/TmsAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.tms_auth_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmsAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
